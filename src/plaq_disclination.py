16d54b79b7a86b0e0baabbcd719472cbc1995f94
mark <mark@mhirsbrunner.com>
2022-06-03 18:03:03 -0500

    Last commit actually split the code into having plaquette- and site-centered disclinations. This commit is fixing an array size typo in th new plaq disc code

--- src/plaq_disclination.py
+++ src/plaq_disclination.py
@@ -226,7 +226,7 @@ def calculate_disclination_rho(nz: int, nx: int, mass: float, phs_mass: float, h
         print('Solving for eigenvectors and eigenvalues')
         evals, evecs = nlg.eigh(h)
 
-    rho = np.zeros((nz, (3 * nx * nx) // 4))
+    rho = np.zeros((nz, (3 * nx * nx) // 4 + nx // 2))
 
     for ii, energy in enumerate(evals):
         if energy <= 0:
@@ -242,68 +242,3 @@ def calculate_disclination_rho(nz: int, nx: int, mass: float, phs_mass: float, h
         pkl.dump(data, handle)
 
     return rho
-
-
-def open_z_hamiltonian(kx: float, ky: float, nz: int, mass: float, phs_mass: float):
-    norb = 8
-
-    h_0 = np.zeros((norb, norb), dtype=complex)
-    h_0_phs = np.zeros_like(h_0)
-    h_z = np.zeros_like(h_0)
-
-    h_0 += (mass + cos(kx) + cos(ky)) * np.kron(gamma_0, sigma_z)
-    h_0 += np.kron(sin(kx) * gamma_x + sin(ky) * gamma_y, sigma_0)
-
-    h_0_phs += phs_mass * np.kron(gamma_5, sigma_0)
-
-    h_z += 1 / 2 * np.kron(gamma_0, sigma_z) + 1j / 2 * np.kron(gamma_z, sigma_0)
-
-    h_z += 1j / 2 * sin(kx) * np.kron(gamma_0, sigma_x) + 1j / 2 * sin(ky) * np.kron(gamma_0, sigma_y)
-
-    h = np.kron(np.identity(nz), np.array(h_0))
-    h[:norb, :norb] += h_0_phs
-    h[-norb:, -norb:] += h_0_phs
-    h += np.kron(np.diag(np.ones(nz - 1), k=1), h_z)
-    h += np.kron(np.diag(np.ones(nz - 1), k=-1), h_z.conj().T)
-
-    return h
-
-
-def calculate_open_z_bands(nz: int, dk: float, mass: float, phs_mass: float, fname='open_z_bands'):
-    norb = 8
-    ks, k_nodes = utils.high_symmetry_lines(dk)
-
-    evals = np.zeros((len(ks), nz * norb))
-
-    for ii, k in enumerate(tqdm(ks)):
-        kx, ky = k
-        h = open_z_hamiltonian(kx, ky, nz, mass, phs_mass)
-        evals[ii] = np.linalg.eigvalsh(h)
-
-    results = evals
-    params = (nz, mass, phs_mass, ks, k_nodes)
-    data = (results, params)
-
-    with open(data_dir / (fname + '.pickle'), 'wb') as handle:
-        pkl.dump(data, handle)
-
-
-def calculate_open_z_dos(nz: int, dk: float, mass: float, phs_mass: float, energy_axis, eta=0.05,
-                         fname='open_z_dos'):
-    ks, k_nodes = utils.high_symmetry_lines(dk)
-
-    dos = np.zeros((len(energy_axis), len(ks)))
-
-    for ii, k in enumerate(tqdm(ks)):
-        kx, ky = k
-        h = open_z_hamiltonian(kx, ky, nz, mass, phs_mass)
-        for jj, energy in enumerate(energy_axis):
-            a = utils.spectral_function(ham=h, energy=energy, eta=eta)
-            dos[jj, ii] = np.sum(np.diag(a)) / (2 * pi)
-
-    results = dos
-    params = (nz, mass, phs_mass, energy_axis, eta, ks, k_nodes)
-    data = (results, params)
-
-    with open(data_dir / (fname + '.pickle'), 'wb') as handle:
-        pkl.dump(data, handle)
